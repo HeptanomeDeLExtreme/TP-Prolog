9f1acf758308185d58b297216e276ceb93261fc4
Omadzu <ophelie.delsaux@hotmail.fr>
2015-10-09 23:29:13 +0200

    Ajout du pion OK
    L'ajout du pion dans la base de faits lorsque la colonne n'est pas vide fonctionne.
    
    Ce qu'il reste à implémenter est la boucle qui redemande un choix à l'IA ou au joueur lorsque celui-ci sélectionne une colonne qui ne peut plus recevoir de jetons !

--- jouerCoup.pl
+++ jouerCoup.pl
@@ -1,3 +1,7 @@
+/* Declarer les predicats dynamiques */
+:- dynamic
+    pion/3.
+    
 /* Base de faits :
 pion(Colonne, Ligne, {1|2}). */
 
@@ -20,20 +24,49 @@ indexDernierPion(Colonne, NumeroLigne) :-
 colonneIndisponible(NumeroLigne, X, Xnew) :-
     NumeroLigne = 6, jouerCoup(X, Xnew, [1, 2]).
 
-/* X represente l''ancien tableau de jeu
+% Permet de connaitre la prochaine case a remplir dans le graphe %
+calculProchaineCase(NumeroLigne, NumeroLigneSuivant) :-
+    NumeroLigneSuivant is NumeroLigne + 1.
+    
+% Permet d ajouter le pion dans la base de faits %
+% NumeroLignePlus1 represente le fait d ajouter en haut de la colonne
+% selectionne par le joueur / l IA le pion.
+ajouterPion(NumeroColonne, NumeroLigneSuivant, Joueur) :-
+    assert(pion(NumeroColonne, NumeroLigneSuivant, Joueur)).
+    
+/* X represente l ancien tableau de jeu
 Xnew represente le nouveau tableau avec le coup joue
-N represente un tableau avec [colonneSelectionnee, numeroJoueur] */
+N represente un tableau avec [colonneSelectionnee, numeroJoueur]
+jouerCoup est une disjonction car il existe deux possibilites :
+- La colonne n est pas disponible pour recevoir un autre jeton. Du coup,
+il faut relancer demander un autre coup et relancer "jouerCoup".
+- La colonne est disponible pour recevoir un jeton supplementaire.
+Dans ce cas il faut simplement ajouter le pion dans le tableau. */
 
+% Cas ou la colonne est indisponible %
 jouerCoup(X, Xnew, [NumeroColonne, Joueur]) :-
     /* Isoler la colonne jouee */
     isolerColonne(NumeroColonne, Colonne),
     
-    /* Recuperer l''index de la prochaine case vide */
+    /* Recuperer l index de la prochaine case vide */
     indexDernierPion(Colonne, NumeroLigne),
     
-    /* Si la colonne est n''est pas pleine, nous pouvons ajouter le nouveau coup
+    /* Si la colonne est n est pas pleine, nous pouvons ajouter le nouveau coup
     dans la colonne */
-    colonneIndisponible(NumeroLigne, X, Xnew),
+    colonneIndisponible(NumeroLigne, X, Xnew).
     
-    /* TODOOOOOOOOOOOOOOOOOOOOOO */
-    not (colonneIndisponible) :- pion(NumeroColonne, NumeroLignePlus1, Joueur), NumeroLignePlus1 is NumeroLigne + 1.
+% Cas ou la colonne est disponible %
+jouerCoup(X, Xnew, [NumeroColonne, Joueur]) :-
+    /* Isoler la colonne jouee */
+    isolerColonne(NumeroColonne, Colonne),
+    
+    /* Recuperer l index de la prochaine case vide */
+    indexDernierPion(Colonne, NumeroLigne),
+    
+    % Si on passe dans ce predicat, c est que la colonne est disponible
+    % pour recevoir un pion (en effet, le predicat colonneIndisponible
+    % retourne faux !). Pas besoin de retester la disponibilite de la colonne.
+    % Donc on ajoute le pion dans la base de faits
+    calculProchaineCase(NumeroLigne, NumeroLigneSuivant),
+    ajouterPion(NumeroColonne, NumeroLigneSuivant, Joueur).
+    
