ace75d1bf6369e4ee9bbf1aa90a6255f4834df0f
mchalumeau <chal.marion@gmail.com>
2015-10-09 11:54:40 +0200

    Update finPartie.pl

--- finPartie.pl
+++ finPartie.pl
@@ -1,3 +1,5 @@
+%Code faux!!!!!!!!!!!!!!!!
+
 pion(1,1,1).
 pion(1,2,1).
 pion(1,3,1).
@@ -10,29 +12,29 @@ pion(5,1,2).
 finjeu :- length(findall(pion(_, 6, _)), 7) , Resultat=0, donnerResultat.
 finjeu :- suite, donnerResultat.
 
-lienVetical(pion(I,J,X), pion(K,L,X)) :- J=L+1, I=K.
-lienVetical(pion(I,J,X), pion(K,L,X)) :- J=L-1, I=K.
+lienVertical(dernPion(I,J,X), pion(K,L,X)) :- J is L+1, I is K.
+lienVertical(dernPion(I,J,X), pion(K,L,X)) :- J is L-1, I is K.
 
-cheminVertical(pion(I,J,X), pion(K,L,X)) :- lienVetical(pion(I,J,X), pion(K,L,X)).
-cheminVertical(pion(I,J,X), pion(K,L,X)) :- lienVetical(pion(I,J,X), pion(M,N,X)), cheminVertical(pion(M,N,X), pion(K,L,X)).
+cheminVertical(dernPion(I,J,X), pion(K,L,X)) :- lienVertical(dernPion(I,J,X), pion(K,L,X)).
+cheminVertical(dernPion(I,J,X), pion(K,L,X)) :- lienVertical(dernPion(I,J,X), pion(M,N,X)), cheminVertical(dernPion(M,N,X), pion(K,L,X)).
 
-lienHorizontal(pion(I,J,X), pion(K,L,X)) :- I=K+1, J=L.
-lienHorizontal(pion(I,J,X), pion(K,L,X)) :- I=K-1, J=L.
+lienHorizontal(dernPion(I,J,X), pion(K,L,X)) :- I is K+1, J is L.
+lienHorizontal(dernPion(I,J,X), pion(K,L,X)) :- I is K-1, J is L.
 
-cheminHorizontal(pion(I,J,X), pion(K,L,X)) :- lienHorizontal(pion(I,J,X), pion(K,L,X)).
-cheminHorizontal(pion(I,J,X), pion(K,L,X)) :- lienHorizontal(pion(I,J,X), pion(M,N,X)), cheminHorizontal(pion(M,N,X), pion(K,L,X)).
+cheminHorizontal(dernPion(I,J,X), pion(K,L,X)) :- lienHorizontal(dernPion(I,J,X), pion(K,L,X)).
+cheminHorizontal(dernPion(I,J,X), pion(K,L,X)) :- lienHorizontal(dernPion(I,J,X), pion(M,N,X)), cheminHorizontal(dernPion(M,N,X), pion(K,L,X)).
 
-lienDiagDroite(pion(I,J,X), pion(K,L,X)) :- J=L+1, I=K+1.
-lienDiagDroite(pion(I,J,X), pion(K,L,X)) :- J=L-1, I=K-1.
+lienDiagDroite(dernPion(I,J,X), pion(K,L,X)) :- J is L+1, I is K+1.
+lienDiagDroite(dernPion(I,J,X), pion(K,L,X)) :- J is L-1, I is K-1.
 
-cheminDiagDroite(pion(I,J,X), pion(K,L,X)) :- lienDiagDroite(pion(I,J,X), pion(K,L,X)).
-cheminDiagDroite(pion(I,J,X), pion(K,L,X)) :- lienDiagDroite(pion(I,J,X), pion(M,N,X)), cheminDiagDroite(pion(M,N,X), pion(K,L,X)).
+cheminDiagDroite(dernPion(I,J,X), pion(K,L,X)) :- lienDiagDroite(dernPion(I,J,X), pion(K,L,X)).
+cheminDiagDroite(dernPion(I,J,X), pion(K,L,X)) :- lienDiagDroite(dernPion(I,J,X), pion(M,N,X)), cheminDiagDroite(dernPion(M,N,X), pion(K,L,X)).
 
-lienDiagGauche(pion(I,J,X), pion(K,L,X)) :- J=L+1, I=K-1.
-lienDiagGauche(pion(I,J,X), pion(K,L,X)) :- J=L-1, I=K+1.
+lienDiagGauche(dernPion(I,J,X), pion(K,L,X)) :- J is L+1, I is K-1.
+lienDiagGauche(dernPion(I,J,X), pion(K,L,X)) :- J is L-1, I is K+1.
 
-cheminDiagGauche(pion(I,J,X), pion(K,L,X)) :- lienDiagGauche(pion(I,J,X), pion(K,L,X)).
-cheminDiagGauche(pion(I,J,X), pion(K,L,X)) :- lienDiagGauche(pion(I,J,X), pion(M,N,X)), cheminDiagGauche(pion(M,N,X), pion(K,L,X)).
+cheminDiagGauche(dernPion(I,J,X), pion(K,L,X)) :- lienDiagGauche(dernPion(I,J,X), pion(K,L,X)).
+cheminDiagGauche(dernPion(I,J,X), pion(K,L,X)) :- lienDiagGauche(dernPion(I,J,X), pion(M,N,X)), cheminDiagGauche(dernPion(M,N,X), pion(K,L,X)).
 
 suite :- length(findall(cheminVertical(dernPion(I,J,X), pion(K,L,X))), 4), Resultat=X.
 suite :- length(findall(cheminHorizontal(dernPion(I,J,X), pion(K,L,X))), 4), Resultat=X.
@@ -41,3 +43,7 @@ suite :- length(findall(cheminDiagGauche(dernPion(I,J,X), pion(K,L,X))), 4), Res
 
 donnerResultat :- not(Resultat==0), write('Felicitation joueur '), write(Resultat), write(', vous avez gagne!').
 donnerResultat :- write('Match nul :(').
+
+%findall(x,...,L)
+%nth0(L+, I, E)
+%length(v)
