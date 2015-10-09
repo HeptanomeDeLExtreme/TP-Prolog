22dd6467f5481d7626e5a98e372a73545286fa90
Nicolas Nadisic <nicolas@nadisic.tk>
2015-10-09 10:40:32 +0200

    Refactorage

--- p4.pl
+++ p4.pl
@@ -9,14 +9,14 @@
 %TODO ajouter le jeu de base, initialiser ni nécessaire
 
 % Le jeu continue tant qu'il n'est pas fini (un gagnant ou plateau plein)
-jouer :- finjeu, !.
+jouer :- finPartie, !.
 
 % Déroulement principal
 jouer :- plateau(X),
 	 afficher(X),
 	 ia(X,N),
 	 jouerCoup(X,Xnouv,N),
-	 jouerEnMem(X,Xnouv),
+	 jouerMemoire(X,Xnouv),
 	 jouer,
 	 retract(plateau(X)),
 	 assert(plateau(Xnouv)).
