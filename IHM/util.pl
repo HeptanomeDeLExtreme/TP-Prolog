bac3e342ff0a52abc2018f91ef6c9b9d3fcf2e52
Omadzu <ophelie.delsaux@hotmail.fr>
2015-10-31 18:54:04 +0100

    Update util.pl
    - Modification de viderPlateau : appel récursif pour vider tous les pions en un appel de viderPlateau.
    - Ajout du prédicat "initialiserPlateau" : Permet d'initialiser le plateau avec les pions de départ.

--- IHM/util.pl
+++ IHM/util.pl
@@ -1,4 +1,6 @@
-/* FONCTIONS UTILITAIRES */
+:- dynamic pion/3.
+
+%% FONCTIONS UTILITAIRES
 
 incrementeX(X,X1):- X1 is X+1.
 
@@ -9,4 +11,15 @@ doubleInc(X,Y,X1,Y1) :- X1 is X+1,Y1 is Y+1.
 ajouterPion(NumeroColonne, NumeroLigneSuivant, Joueur) :-
     assert(pion(NumeroColonne, NumeroLigneSuivant, Joueur)).
 
-viderPlateau :- retract(pion(X,Y,1)),retract(pion(U,V,2)).
+viderPlateau :- retract(pion(Colonne, Ligne, Joueur)), pion(Colonne, Ligne, Joueur), viderPlateau.
+
+initialiserPlateau :-
+%% pion(Colonne, Ligne, Joueur)
+	assert(pion(1,0,-10)),
+	assert(pion(2,0,-10)),
+	assert(pion(3,0,-10)),
+	assert(pion(4,0,-10)),
+	assert(pion(5,0,-10)),
+	assert(pion(6,0,-10)),
+	assert(pion(7,0,-10)),
+	assert(pion(-10,-10,-10)).
