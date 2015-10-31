d4aaac6872bc9c972eb20be4cbf3b2a738b44954
niosega <nicolas.bonfante@insa-lyon.fr>
2015-10-31 19:54:16 +0100

    modif de evaluation

--- IHM/evaluation.pl
+++ IHM/evaluation.pl
@@ -1,34 +1,39 @@
-% aléatoire vs offensive
+initEval :- nb_setval('NBEval',10).
 
-eval1 :- initEval1,combatIAEval1.
-initEval1 :- nb_setval('AVSO',0).
+calculPourcentage(PJ1,PJ2,NBJ1,NBJ2) :- nb_getval('NBEval',N),PJ1 is (100*NBJ1/N),PJ2 is (100*NBJ2/N).
+
+% aléatoire vs offensive
+eval1 :- nb_getval('NBEval',N),initEval1,between(1,N,I),combatIAEval1.
+
+initEval1 :- nb_setval('AVSO1',0),nb_setval('AVSO2',0).
 
 combatIAEval1 :- tourIA1Eval1.
 
 testIA1Eval1(N) :- isolerColonne(N, Colonne),
 		indexDernierPion(Colonne, NumeroLigne),
 		(
-		gagne(N,NumeroLigne,1) -> nb_getval('AVSO',X),incrementeX(X,X1),nb_setval('AVSO',X1),write('J1\n');
+		gagne(N,NumeroLigne,1) -> nb_getval('AVSO1',X),incrementeX(X,X1),nb_setval('AVSO1',X1),write('aleatoire'),viderPlateau;
 		tourIA2Eval1
 		).
 		
 testIA2Eval1(N) :- isolerColonne(N, Colonne),
 		indexDernierPion(Colonne, NumeroLigne),
 		(
-		gagne(N,NumeroLigne,2) -> write('J2\n');
+		gagne(N,NumeroLigne,2) -> nb_getval('AVSO2',X),incrementeX(X,X1),nb_setval('AVSO2',X1),write('offensive'),viderPlateau;
 		tourIA1Eval1
 		).
 		
-tourIA1Eval1 :-	write('tour1\n'),
-			%ia(N),
-			iADefensive(1,N),
+tourIA1Eval1 :-	write('tour1'),
+			ia(N),
 			jouerCoup([N,1]),
 			testIA1Eval1(N),!.
 			
-tourIA2Eval1 :-	write('tour2\n'),
+tourIA2Eval1 :-	write('tour2'),
 			iAOffensive(2,M),
 			jouerCoup([M,2]),
 			testIA2Eval1(M),!.  
+			
+pourcentage :- nb_getval('AVSO1',NBJ1),nb_getval('AVSO2',NBJ2),calculPourcentage(PJ1,PJ2,NBJ1,NBJ2),write(PJ1),write(' '),write(PJ2).
 
 
 % aléatoire vs defensive
--- IHM/util.pl
+++ IHM/util.pl
@@ -1,6 +1,4 @@
-:- dynamic pion/3.
-
-%% FONCTIONS UTILITAIRES
+/* FONCTIONS UTILITAIRES */
 
 incrementeX(X,X1):- X1 is X+1.
 
@@ -11,15 +9,4 @@ doubleInc(X,Y,X1,Y1) :- X1 is X+1,Y1 is Y+1.
 ajouterPion(NumeroColonne, NumeroLigneSuivant, Joueur) :-
     assert(pion(NumeroColonne, NumeroLigneSuivant, Joueur)).
 
-viderPlateau :- retract(pion(Colonne, Ligne, Joueur)), pion(Colonne, Ligne, Joueur), viderPlateau.
-
-initialiserPlateau :-
-%% pion(Colonne, Ligne, Joueur)
-	assert(pion(1,0,-10)),
-	assert(pion(2,0,-10)),
-	assert(pion(3,0,-10)),
-	assert(pion(4,0,-10)),
-	assert(pion(5,0,-10)),
-	assert(pion(6,0,-10)),
-	assert(pion(7,0,-10)),
-	assert(pion(-10,-10,-10)).
+viderPlateau :- retract(pion(X,Y,1)),retract(pion(U,V,2)).
