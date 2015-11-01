7cf60348ea889bba5303f09f6c83b6785149a5b9
Nicolas Nadisic <nicolas@nadisic.tk>
2015-11-01 17:28:33 +0100

    Debugage partiel de ia2FenouilSec

--- IHM/ia2FenouilSec.pl
+++ IHM/ia2FenouilSec.pl
@@ -4,6 +4,8 @@
 :- include('jouerCoup.pl').
 :- include('debug.pl').
 :- include('evaluation.pl').
+:- include('util.pl').
+:- include('pionsFictifs.pl').
 
 % IA Fenouil Sec
 % Recherche du coup le plus pertinent dans l'immédiat
@@ -19,7 +21,14 @@
 %%%%%%%%%%%%%%%%%%%% Prog principal %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-zbla(N) :- ia(N).
+% Base de faits de tests, à enlever par la suite
+pion(1,1,1).
+pion(2,1,1).
+pion(3,1,1).
+pion(1,2,2).
+
+
+zblaFenouil(N) :- ia(N).
 
 stop :- true.
 
@@ -32,30 +41,36 @@ stop :- true.
 % entraine la victoire adverse
 %iaFS(Col) :- peutPerdre(pion(Col,Li,2)), var(Col).
 
-% tente de rallonger un de ses chemins de 2 si possible, puis de bloquer un chemin de 2 de l'adversaire, et idem avec les pions unitaires.
- 
- iaFS(Col) :-(
- 	     testInsertion2C(2,Col)->stop ;
- 	     testInsertion2L(2,Col)->stop ;
- 	     testInsertion2DG(2,Col)->stop ;
- 	     testInsertion2C(1,Col)->stop ;
- 	     testInsertion2L(1,Col)->stop ;
- 	     testInsertion2DG(1,Col)->stop ;
- 	     testInsertionPion(2,Col)->stop ;
- 	     testInsertionPion(1,Col)->stop ;
- 	     zbla(Col)).
+% Partie 2 de l'IA, si on ne peut ni gagner ni empêcher l'autre de gagner au
+% tour suivant 
+% Tente de rallonger un de ses chemins de 2 si possible, puis de bloquer un
+% chemin de 2 de l'adversaire, et idem avec les pions unitaires. 
+ %% iaFS(Col) :-(
+ %% 	     testInsertion2C(2,Col)->stop ;
+ %% 	     testInsertion2L(2,Col)->stop ;
+ %% 	     testInsertion2DG(2,Col)->stop ;
+ %% 	     testInsertion2C(1,Col)->stop ;
+ %% 	     testInsertion2L(1,Col)->stop ;
+ %% 	     testInsertion2DG(1,Col)->stop ;
+ %% 	     testInsertionPion(2,Col)->stop ;
+ %% 	     testInsertionPion(1,Col)->stop ;
+ %% 	     zbla(Col)).
 
 %%%%%%%%%%%%%%%%%%%% Sous-prédicats %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %retirerPion
-retirerPion(Col) :- isolerColonne(Col, Colonne),indexDernierPion(Colonne, NumeroLigne), retract(pion(Col,NumeroLigne,_)).
+retirerPion(Col) :- isolerColonne(Col, Colonne),
+		    indexDernierPion(Colonne, NumeroLigne),
+		    retract(pion(Col,NumeroLigne,_)).
 
 %peutJouer est vrai si il est possible de jouer sur la col N (col non pleine)
 peutJouer(N):- \+ pion(N, 6, 1).
 
 %peutGagner
-peutGagner(pion(Col,Li,Joueur)) :- between(1,7,ColCour),jouerCoup([ColCour,Joueur]),isolerColonne(ColCour, Colonne),indexDernierPion(Colonne, NumeroLigne),(gagneTest(X,NumeroLigne,Joueur) -> Col=ColCour,retirerPion(Col),!;retirerPion(Col)),nonvar(Col).
+peutGagner(pion(Col,Li,Joueur)) :- between(1,7,ColCour),
+				   jouerCoup([ColCour,Joueur]),
+				   isolerColonne(ColCour, Colonne),
+				   indexDernierPion(Colonne, NumeroLigne),
+				   (gagne(X,NumeroLigne,1) -> Col=ColCour,retirerPion(Col),!;retirerPion(Col)),nonvar(Col).
 
-%peutPerdre 
-peutPerdre(pion(Col,Li,Joueur)) :- between(1,7,ColCour),JoueurAdvs is 3-Joueur,jouerCoup([ColCour,JoueurAdvs]),isolerColonne(ColCour, Colonne),indexDernierPion(Colonne, NumeroLigne),(gagneTest(X,NumeroLigne,JoueurAdvs) -> Col=ColCour,retirerPion(Col),!;retirerPion(Col)), nonvar(Col).
