eae6bac6bd0e9aa3939b7ec242a7403248c94117
Omadzu <ophelie.delsaux@hotmail.fr>
2015-11-02 14:22:38 +0100

    Update util.pl
    Modification du prédicat pour vider le plateau.

--- IHM/util.pl
+++ IHM/util.pl
@@ -9,6 +9,17 @@ doubleInc(X,Y,X1,Y1) :- X1 is X+1,Y1 is Y+1.
 ajouterPion(NumeroColonne, NumeroLigneSuivant, Joueur) :-
     assert(pion(NumeroColonne, NumeroLigneSuivant, Joueur)).
 
-viderPlateau :- retract(pion(_,_,1)),retract(pion(_,_,2)).
+testVidePlateau1 :- (
+                   retract(pion(_,_,1)) -> testVidePlateau1;
+                   true
+                   ).
+
+testVidePlateau2 :- (
+    retract(pion(_,_,2)) -> testVidePlateau2;
+    true
+    ).
+
+% Méthode pour vider le plateau
+testVidePlateau :- testVidePlateau1,testVidePlateau2.
 
 debug(S) :- jpl_call('main',debug,[S],_).
