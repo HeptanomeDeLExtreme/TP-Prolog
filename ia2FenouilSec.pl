769cad8f4fa969c50bee8ad4ab401e5bff54bf8a
Cedic <ced742@gmail.com>
2015-11-05 08:39:19 +0100

    Màj IA Mixte

--- ia2FenouilSec.pl
+++ ia2FenouilSec.pl
@@ -1,16 +1,17 @@
 % peutGagner est vrai si ce pion entraine la victoire du joueur J
 % Unifie Col à la colonne sur laquelle jouer
-iaFS(J,Col) :- peutGagner(Col,J).
+iaMixte(J,Col) :- peutGagner(Col,J).
 
 % peutPerdre est vrai si ce pion, de l'autre couleur, 
 % entraine la victoire adverse
-iaFS(J,Col) :- peutPerdre(Col,J).
+iaMixte(J,Col) :- peutPerdre(Col,J).
 
-% Partie 2 de l'IA, si on ne peut ni gagner ni empêcher l'autre de gagner au
-% tour suivant 
-% Tente de rallonger un de ses chemins de 2 si possible, puis de bloquer un
-% chemin de 2 de l'adversaire, et idem avec les pions unitaires. 
-iaFS(J,Col) :- (
+% Partie 2 de l'IA, si on ne peut ni gagner ni empêcher l'autre de gagner 
+% au tour suivant 
+% Tente de bloquer un chemin adverse de taille 2 si possible, puis de
+%  rallonger ses propres chemins, et répete le même procédé pour les
+% pions unitaires
+iaMixte(J,Col) :- (
  		  testInsertion2C(J,Col)->stop ;
  		  testInsertion2L(J,Col)->stop ;
  		  testInsertion2DG(J,Col)->stop ;
@@ -40,16 +41,16 @@ peutGagnerSurCol(Col) :- jouerCoup([Col,1]),
             retirerPion(Col),!,false).
 
 % Predicat qui renvoit true si le joueur J peut gagner sur la colonne Col 
-heyJpeuxGagner(Col,J) :- peutGagnerSurCol(1), Col is 1.
-heyJpeuxGagner(Col,J) :- peutGagnerSurCol(2), Col is 2.
-heyJpeuxGagner(Col,J) :- peutGagnerSurCol(3), Col is 3.
-heyJpeuxGagner(Col,J) :- peutGagnerSurCol(4), Col is 4.
-heyJpeuxGagner(Col,J) :- peutGagnerSurCol(5), Col is 5.
-heyJpeuxGagner(Col,J) :- peutGagnerSurCol(6), Col is 6.
-heyJpeuxGagner(Col,J) :- peutGagnerSurCol(7), Col is 7.
+checkVictoireColonne(Col,J) :- peutGagnerSurCol(1), Col is 1.
+checkVictoireColonne(Col,J) :- peutGagnerSurCol(2), Col is 2.
+checkVictoireColonne(Col,J) :- peutGagnerSurCol(3), Col is 3.
+checkVictoireColonne(Col,J) :- peutGagnerSurCol(4), Col is 4.
+checkVictoireColonne(Col,J) :- peutGagnerSurCol(5), Col is 5.
+checkVictoireColonne(Col,J) :- peutGagnerSurCol(6), Col is 6.
+checkVictoireColonne(Col,J) :- peutGagnerSurCol(7), Col is 7.
 
 % Predicat qui unifie Col avec la colonne qui fait gagner le joueur J
-peutGagner(Col,J) :- heyJpeuxGagner(Col,J),!.
+peutGagner(Col,J) :- checkVictoireColonne(Col,J),!.
 
 % Predicat qui unifie Col avec la colonne qui fait perdre le joueur J
-peutPerdre(Col,J) :- heyJpeuxGagner(Col,3-J),!.
+peutPerdre(Col,J) :- checkVictoireColonne(Col,3-J),!.
